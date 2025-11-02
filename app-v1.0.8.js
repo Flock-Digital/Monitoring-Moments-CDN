f92f01790da0069e626298e384a5872992a52efd
Flock Digital <162156388+FlockDigitalLtd@users.noreply.github.com>
2025-11-02 23:33:53 +0000

    Update and rename app-v1.0.7.js to app-v1.0.8.js

--- app-v1.0.8.js
+++ app-v1.0.8.js
@@ -2561,4 +2561,8 @@ function init() {
 	addEventListeners();
 }
 
-document.addEventListener('DOMContentLoaded', init);
+if (document.readyState !== 'loading') {
+	init();
+} else {
+	document.addEventListener('DOMContentLoaded', init);
+}
